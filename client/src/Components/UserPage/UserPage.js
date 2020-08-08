bf6bc67b6e62e02ef2fa6d0200ea2e31adfb57c8
SHorne <swhorne@gmail.com>
2020-08-08 13:11:15 -0600

    adding images and components

---
+++ client/src/Components/UserPage/UserPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./UserPage.css";
+
+
+
+function LandingPage() {
+    return(
+        <div>
+            <h1>Landing Page</h1>
+        </div>
+    )
+}
