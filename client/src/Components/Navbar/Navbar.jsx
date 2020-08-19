7c80cca1933213aedc86e20d7a2247922b2d1f66
pmarti99 <parkerjmartin883@gmail.com>
2020-08-18 21:56:43 -0600

    adding navbar changes

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -16,11 +16,7 @@ function Navbar() {
         variant="light"
         id="header"
       >
-<<<<<<< HEAD
         <ReactBootstrap.Navbar.Brand href='/'>
-=======
-        <ReactBootstrap.Navbar.Brand href="/LandingPage">
->>>>>>> origin
           <img
             src="./Images/altLogo.png"
             alt="Alternate logo"
