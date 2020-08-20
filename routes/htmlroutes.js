38d76821bc772616d725e0a6c535f5761a17946c
pmarti99 <parkerjmartin883@gmail.com>
2020-08-20 17:51:12 -0600

    commiting

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -26,7 +26,18 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../client/src/Components/Login.Login.jsx"));
         // res.sendFile(path.join(__dirname, "../src/pages/login.html"));
       });
-      
+      app.get("/RecipeSearch", isAuthenticated, (req, res)=>{
+        res.render(("RecipeSearch"))
+
+      })
+      app.get("/UserPage", isAuthenticated, (req, res)=>{
+        res.render(("UserPage"))
+
+      })
+      app.get("/IngredientSearch", isAuthenticated, (req, res)=>{
+        res.render(("IngredientSearch"))
+
+      })
     
       // Here we've add our isAuthenticated middleware to this route.
       // If a user who is not logged in tries to access this route they will be redirected to the signup page
