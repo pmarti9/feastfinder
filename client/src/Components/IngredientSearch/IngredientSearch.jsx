644759083e7c096c4e78fac56136677cf9a46ff0
Sam Horne <swhorne@gmail.com>
2020-08-20 18:35:11 -0600

    update ingredients.jsx

--- client/src/Components/IngredientSearch/IngredientSearch.jsx
+++ client/src/Components/IngredientSearch/IngredientSearch.jsx
@@ -15,7 +15,7 @@ import App from "../../App";
 import logo from '../../logo.svg'
 
 const request = require('request');
-const qs = require('qs');
+const qs = require('qs')
 
 function IngredientSearch() {
 
@@ -41,93 +41,43 @@ function IngredientSearch() {
     if (error) throw new Error(error);
     console.log(body);
     console.log(options);
+    console.log(response);
   });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+<div>
+          <ReactBootstrap.Container
+            className="justify-content-md-center"
+            id="recipeSearchContainer"
           >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-//   const [Recipes, setRecipe] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const getRecipeData = async () => {
-//     try {
-//       const data = await axios.get(
-//         "https://nba-players.herokuapp.com/players-stats"
-//       );
-//       console.log(data);
-//       setRecipe(data.data);
-//       setLoading(true);
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-//   const columns = [
-//     { keyField: "id", integer: "id" },
-//     { dataField: "recipe", text: "Recipe Name" },
-//     { dataField: "author", text: "Author" },
-//     { dataField: "picture", text: "Picture" },
-//   ];
-
-//   useEffect(() => {
-//     getRecipeData();
-//   }, []);
-
-//   return (
-//     <div className="IngredientSearch">
-//       <ReactBootstrap.Container id='resultsContainer'>
-//       {loading ? (
-//         <BootstrapTable id='resultsTable'
-//           keyField="recipe"
-//           data={Recipes}
-//           columns={columns}
-//           pagination={paginationFactory()}
-//           />
-//       ) : (
-//         <ReactBootstrap.Spinner animation="border" />
-//       )}
-//           </ReactBootstrap.Container>
-//     </div>
-//   );
-// };
-
-// return (
-//   <div>
-//     <ReactBootstrap.Container
-//       id="ingredientSearchContainer"
-//       className="justify-content-md-center">
-//         <ReactBootstrap.Table className="table table-bordered table-responsive" id='ingredientSearchTable'>
-//           <ReactBootstrap.thead>
-//             <ReactBootstrap.tr>
-//               <ReactBootstrap.th scope='col'>Search Ingredients</ReactBootstrap.th>
-//               <ReactBootstrap.th scope='col'>
-//                 <ReactBootstrap.Button variant="primary">Add Ingredient</ReactBootstrap.Button>
-//               </ReactBootstrap.th>
-//             </ReactBootstrap.tr>
-//           </ReactBootstrap.thead>
-//           <ReactBootstrap.TBody id='ingredientSearchTableBody'>
-//             <ReactBootstrap.tr>
-
-//             </ReactBootstrap.tr>
-//           </ReactBootstrap.TBody>
-
-//         </ReactBootstrap.Table>
-//     </ReactBootstrap.Container>
-//   </div>
-// );
-//}
-
+            <h1 id="recipeSearchHeader">Search for recipes by ingredient(s).</h1>
+            <ReactBootstrap.Form className="recipeSearchForm" id="recipeSearchFormID">
+              <ReactBootstrap.FormGroup
+                controlId="formRecipeSearch"
+                id="recipeSearchFormGrp"
+              >
+                <ReactBootstrap.InputGroup
+                  className="mb-3"
+                  id="recipeSearchInputGroup"
+                >
+                  <ReactBootstrap.InputGroup.Prepend id="searchIconPrepend">
+                    <ReactBootstrap.Button
+                      variant="outline-secondary"
+                      id="searchBtn"
+                    >
+                      <img
+                        src={require('../../Images/Search_Icon_Orange.png')}
+                        id="searchIcon"
+                      />
+                    </ReactBootstrap.Button>
+                  </ReactBootstrap.InputGroup.Prepend>
+                  <ReactBootstrap.FormControl aria-describedby="basic-addon1" />
+                </ReactBootstrap.InputGroup>
+              </ReactBootstrap.FormGroup>
+            </ReactBootstrap.Form>
+            <p id="commaSearch">Separate each ingredient with a comma</p>
+          </ReactBootstrap.Container>
+        </div>
+      );
+    }
 export default IngredientSearch;
